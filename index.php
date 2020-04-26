7fc8f4daff8a7540f2835fcc8f8772e3eb69aa0f
Aleksander <adishnica@yahoo.com>
2020-04-26 16:40:13 +0200

    Fixes

--- index.php
+++ index.php
@@ -11,16 +11,16 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/' :
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/app/views/main.php';
         break;
     case '' :
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/app/views/main.php';
         break;
     case '/about' :
-        require __DIR__ . '/views/about.php';
+        require __DIR__ . '/app/views/main.php';
         break;
     default:
         http_response_code(404);
-        require __DIR__ . '/views/404.php';
+        require __DIR__ . '/app/views/main.php';
         break;
 }
