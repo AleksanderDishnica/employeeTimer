1f72c387f132952eaceb588b2e490b93b0d183fe
Aleksander <adishnica@yahoo.com>
2020-04-26 16:38:40 +0200

    Added simple router

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
+/*
 	require_once 'autoload.php';
 
 	Header::show([
 		'title'=>'Track Employees',
-	]);
+	]); 
+*/
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
